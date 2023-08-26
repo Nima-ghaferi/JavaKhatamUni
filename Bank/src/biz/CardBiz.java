d1e348b7304cebdbf5d6b2c60a9861923854d181
Aram2811 <a.javanmardy2004@gmail.com>
2023-08-26 13:26:51 +0330

    card complete.

--- Bank/src/biz/CardBiz.java
+++ Bank/src/biz/CardBiz.java
@@ -1,19 +1,14 @@
 package biz;
 
-import entity.Card;
-import entity.CardType;
-import entity.CardState;
-import entity.Account;
-import entity.Customer;
+import entity.*;
 import main.Data;
 
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
-import java.util.Random;
-import java.util.Scanner;
+import java.time.LocalDate;
+import java.util.*;
 
 public class CardBiz {
     private static int generatedCardId = 0;
+    private static List<String> generatedCardPANs = new ArrayList<>();
     private static Random random = new Random();
 
     public static void addCard() {
@@ -21,26 +16,38 @@ public class CardBiz {
 
         int cardId = generateCardId();
 
+        String cardPassword = generateCardPassword();
+
+        Date cardCreationDate = Date.from(LocalDate.now().atStartOfDay()
+                .atZone(java.time.ZoneId.systemDefault())
+                .toInstant());
+
+        Date cardExpirationDate = getExpirationDate(cardCreationDate);
+
+        String CardCvv2 = generateCVV2();
+
+        System.out.print("Enter card holder for card: ");
+        Customer cardHolder = CustomerBiz.findById(scanner.nextInt());
+
         System.out.print("Enter account id: ");
         Account defaultAccount = AccountBiz.findById(scanner.nextInt());
 
-        String cardPAN = generateCardPAN(defaultAccount.getAccountId(), cardId);
-        String cardPassword = generateCardPassword();
-        LocalDateTime cardCreationDate = LocalDateTime.now();
-        LocalDateTime cardExpirationDate = cardCreationDate.plus(5, ChronoUnit.YEARS);
-        String CardCvv2 = generateCVV2();
-        int customerId = Integer.parseInt(scanner.next());
-        Customer cardHolder = CustomerBiz.findById(customerId);
+        assert defaultAccount != null;
+        String cardPAN = generateCardPAN(Integer.parseInt(defaultAccount.getAccountNumber()));
 
+        System.out.println("Card Types:");
         System.out.println("1. Debit");
+        System.out.print("Choose type of card: ");
         int choice = scanner.nextInt();
         if(choice == 1) {
             CardType cardType = CardType.DEBIT;
         }
 
+        System.out.println("Card States: ");
         System.out.println("1. Active");
         System.out.println("2. Hot");
         System.out.println("3. Expire");
+        System.out.print("Choose state of card: ");
         choice = scanner.nextInt();
         if(choice == 1){
             CardState cardStatus = CardState.ACTIVE;
@@ -52,20 +59,28 @@ public class CardBiz {
             CardState cardStatus = CardState.EXPIRE;
         }
 
-        Card card = new Card(cardId, cardPAN, cardPassword, cardCreationDate, cardExpirationDate, CardCvv2, cardHolder, defaultAccount, cardType, );
-        account.setDefaultCard(card);
-
-        Data.cards.add(card);
-        return card;
     }
 
     private static int generateCardId() {
-        return generatedCardId++;
+        generatedCardId++;
+        return generatedCardId;
     }
 
-    private static String generateCardPAN(int accountId, int cardId) {
+    private static String getRandomCardPAN(int accountNumber) {
+        String firstPart = "5022";
+        String secondPart = String.valueOf(random.nextLong(1000,10000));
+        String thirdPart = String.valueOf(accountNumber % Math.pow(10,8));
+        return String.format("%s%s%s",firstPart, secondPart, thirdPart);
     }
 
+    private static String generateCardPAN(int accountNumber){
+        String generatedCardPAN = getRandomCardPAN(accountNumber);
+        if (generatedCardPANs.contains(generatedCardPAN)) {
+            do generatedCardPAN = getRandomCardPAN(accountNumber);
+            while (generatedCardPANs.contains(generatedCardPAN));
+        }
+        return generatedCardPAN;
+    }
     private static String generateCardPassword() {
         int password = random.nextInt(10000);
         return String.format("%04d", password);
@@ -74,6 +89,13 @@ public class CardBiz {
     private static String generateCVV2() {
         int cvv2 = random.nextInt(1000);
         return String.format("%03d", cvv2);
+    }
+
+    private static Date getExpirationDate(Date createDate){
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(createDate);
+        calendar.add(Calendar.YEAR, 5);
+        return calendar.getTime();
     }
 }
 
