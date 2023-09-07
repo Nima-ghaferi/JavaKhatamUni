f0459a244ae3938b49362960ae357d09abce3275
Reza laamian <rezalaamian@gmail.com>
2023-09-07 10:09:56 +0330

    modify main for call get getBalance method

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,9 +9,9 @@ public class Main {
         while(true){
             System.out.println("What do you want to do: ");
             System.out.println("1. Add customer");
-            System.out.println("2. Add Account");
-            System.out.println("3.");
-            System.out.println("4.");
+            System.out.println("2. Add account");
+            System.out.println("3. Add card");
+            System.out.println("4. Get balance");
             System.out.println("5.");
             System.out.println("6.");
             System.out.println("q. Quit");
@@ -28,6 +28,10 @@ public class Main {
 
             else if(choice.equalsIgnoreCase("3")) {
                 CardBiz.addCard();
+            }
+
+            else if(choice.equalsIgnoreCase("4")) {
+                CardBiz.getBalance();
             }
 
             else if(choice.equalsIgnoreCase("q")) {
