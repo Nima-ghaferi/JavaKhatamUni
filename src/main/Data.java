d8de4ea2ffc97a594c4c069ad65bc58d21f72a44
Ali Khadempour <ali.khadempour@outlook.com>
2023-09-11 16:18:51 +0330

    optimize Data

--- src/main/Data.java
+++ src/main/Data.java
@@ -2,18 +2,19 @@ package main;
 
 import entity.Account;
 import entity.Card;
-import entity.CardType;
 import entity.Customer;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Data {
-    public static List<Customer> customers = new ArrayList<>();
-    public static List<Account> accounts = new ArrayList<>();
+    public static List<Customer> customers;
+    public static List<Account> accounts;
+    public static List<Card> cards;
 
-    public static List<Card> cards = new ArrayList<>();
-    public Data(){
-
+    static{
+        customers = new ArrayList<>();
+        accounts = new ArrayList<>();
+        cards = new ArrayList<>();
     }
 }
