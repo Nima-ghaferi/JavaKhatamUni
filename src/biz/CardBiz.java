95c707590b8e2c0309de91507cd53760e17926cd
Ali Khadempour <ali.khadempour@outlook.com>
2023-09-13 12:09:06 +0330

    update Card and CardBiz

--- src/biz/CardBiz.java
+++ src/biz/CardBiz.java
@@ -7,11 +7,17 @@ import java.math.BigDecimal;
 import java.util.*;
 
 public class CardBiz {
-    private static int generatedCardId = 0;
-    private static List<String> generatedCardPANs = new ArrayList<>();
-    private static final Random random = new Random();
+    private static int cardId;
+    private static List<String> cardPANs;
+    private static final Random random;
 
-    public static void addCard() {
+    static {
+        cardId = 0;
+        cardPANs = new ArrayList<>();
+        random = new Random();
+    }
+
+    public static void addCard() throws Exception {
         Scanner scanner = new Scanner(System.in);
 
         int cardId = generateCardId();
@@ -20,31 +26,38 @@ public class CardBiz {
 
         Date cardCreationDate = new Date();
 
-        Date cardExpirationDate = getExpirationDate(cardCreationDate);
+        Date cardExpiryDate = generateExpiryDate(cardCreationDate);
 
-        String CardCvv2 = generateCVV2();
+        String cardCvv2 = generateCVV2();
 
-        System.out.print("Enter card holder id: ");
+        System.out.print("Enter card holder ID: ");
         Customer cardHolder = CustomerBiz.findById(scanner.nextInt());
-
-        System.out.print("Enter account id: ");
-        Account cardDefaultaccount = AccountBiz.findById(scanner.nextInt());
-
-        String cardPAN = null;
-        if (cardDefaultaccount != null) {
-            cardPAN = generateCardPAN(Integer.parseInt(cardDefaultaccount.getAccountNumber()));
+        if(cardHolder == null){
+            throw new Exception();
         }
+
+        System.out.print("Enter account ID: ");
+        Account cardAccount = AccountBiz.findById(scanner.nextInt());
+        if(cardAccount == null){
+            throw new Exception();
+        }
+
+        String cardPAN = generateCardPAN(Long.parseLong(cardAccount.getAccountNumber()));
 
         System.out.println("Card Types:");
         System.out.println("1. Debit");
+        System.out.println("2. Credit");
         System.out.print("Choose type of card: ");
-        int choice = scanner.nextInt();
-        CardType cardType = null;
-        if(choice == 1) {
+        String choiceType = scanner.next();
+        CardType cardType;
+        if(choiceType.equalsIgnoreCase("1")) {
              cardType = CardType.DEBIT;
         }
+        else if(choiceType.equalsIgnoreCase("2")){
+            cardType = CardType.CREDIT;
+        }
         else{
-            System.out.println("invalid input!");
+            throw new Exception();
         }
 
         System.out.println("Card States: ");
@@ -52,62 +65,61 @@ public class CardBiz {
         System.out.println("2. Hot");
         System.out.println("3. Expire");
         System.out.print("Choose state of card: ");
-        choice = scanner.nextInt();
-        CardState cardStatus = null;
-        if(choice == 1){
+        String choiceState = scanner.next();
+        CardState cardStatus;
+        if(choiceState.equalsIgnoreCase("1")){
              cardStatus = CardState.ACTIVE;
         }
-        else if(choice == 2){
+        else if(choiceState.equalsIgnoreCase("2")){
             cardStatus = CardState.HOT;
         }
-        else if(choice == 3){
+        else if(choiceState.equalsIgnoreCase("3")){
             cardStatus = CardState.EXPIRE;
         }
         else{
-            System.out.println("invalid input!");
+            throw new Exception();
         }
 
-        Data.cards.add(new Card(cardId, cardPAN, cardPassword, cardCreationDate, cardExpirationDate, CardCvv2, cardHolder, cardDefaultaccount, cardType, cardStatus));
-
+        Data.cards.add(new Card(cardId, cardPAN, cardPassword, cardCreationDate, cardExpiryDate, cardCvv2, cardHolder,
+                                cardAccount, cardType, cardStatus));
     }
-    public static BigDecimal getBalance(){
-        System.out.println("Enter card PAN: ");
+
+    public static BigDecimal getBalance() throws Exception {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter card PAN: ");
         String cardPAN = scanner.next();
 
         Card card = CardBiz.findByPAN(cardPAN);
         if(card != null) {
             System.out.println("Enter card password: ");
             if(checkPassword(card, scanner.next())){
-                return card.getDefaultAccount().getBalance();
+                return card.getAccount().getBalance();
             }
             else{
-                System.out.println("Password is invalid!");
+                throw new Exception();
             }
         }
         else{
-            System.out.println("Card PAN is invalid!");
+            throw new Exception();
         }
-        return null;
     }
-
     private static int generateCardId() {
-        generatedCardId++;
-        return generatedCardId;
+        cardId++;
+        return cardId;
     }
 
-    private static String getRandomCardPAN(int accountNumber) {
+    private static String getRandomCardPAN(long accountNumber) {
         String firstPart = "5022";
-        String secondPart = String.valueOf(random.nextLong(1000,10000));
+        String secondPart = String.format("%04d", random.nextLong(10000));
         String thirdPart = String.valueOf(accountNumber % Math.pow(10,8));
         return String.format("%s%s%s",firstPart, secondPart, thirdPart);
     }
 
-    private static String generateCardPAN(int accountNumber){
+    private static String generateCardPAN(long accountNumber){
         String generatedCardPAN;
         do generatedCardPAN = getRandomCardPAN(accountNumber);
-        while (generatedCardPANs.contains(generatedCardPAN));
-        generatedCardPANs.add(generatedCardPAN);
+        while (cardPANs.contains(generatedCardPAN));
+        cardPANs.add(generatedCardPAN);
         return generatedCardPAN;
     }
 
@@ -121,7 +133,7 @@ public class CardBiz {
         return String.format("%03d", cvv2);
     }
 
-    private static Date getExpirationDate(Date createDate){
+    private static Date generateExpiryDate(Date createDate){
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(createDate);
         calendar.add(Calendar.YEAR, 5);
@@ -130,7 +142,7 @@ public class CardBiz {
 
     public static Card findByPAN(String cardPAN) {
         for(Card card: Data.cards) {
-            if (card.getCardPAN().equalsIgnoreCase(cardPAN)) {
+            if (card.getPAN().equalsIgnoreCase(cardPAN)) {
                 return card;
             }
         }
